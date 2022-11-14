bbf599982567d335399ed1479057b59beae981a5
alexforsale <alexforsale@yahoo.com>
2022-11-14 11:25:42 +0700

    update

--- modules.lisp
+++
@@ -1,8 +0,0 @@
-(let ((module-dir
-        (or (probe-file "/usr/share/stumpwm/contrib")
-            (probe-file "/usr/local/share/stumpwm/contrib")
-            (probe-file (merge-pathnames ".local/share/stumpwm/contrib"
-                                         (user-homedir-pathname)))
-            (probe-file (merge-pathnames ".config/stumpwm/modules"
-                                         (user-homedir-pathname))))))
-  (set-module-dir module-dir))
